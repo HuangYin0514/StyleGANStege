633ad6e3dc4ab59ab1fe5cc0f83bc6a3defc9a06
Huang Yin <1071324110@qq.com>
2020-05-19 17:16:34 +0800

    1

--- test_module/test_checkpoint.py
+++ test_module/test_checkpoint.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     checkpoint = '/home/hy/vscode/StyleGANStege/experiments/ExtractNet'
     model = build_model('ExtractNet', image_size=64)
     model = checkpointNet.load_part_network(model, checkpoint, 'final')
-    print(model)
+    # print(model)
--- utils/checkpointNet.py
+++ utils/checkpointNet.py
@@ -51,24 +51,49 @@ def load_part_network(network, path, epoch_label):
 
     # devie---------------------------------------------------------------------------
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    #file name -----------------------------------------------------------------------------
+
+    # file name -----------------------------------------------------------------------------
     file_path = os.path.join(path, 'net_%s.pth' % epoch_label)
 
     # Original saved file with DataParallel-------------------------------------------------------
     state_dict = torch.load(file_path, map_location=torch.device(device))
 
-    # If the model saved with DataParallel, the keys in state_dict contains 'module'-------------
-    if list(state_dict.keys())[0][:6] == 'module':
-        # Create new OrderedDict that does not contain `module.`
-        new_state_dict = OrderedDict()
-        for k, v in state_dict.items():
-            key_name = k[7:]  # remove `module.`
-            new_state_dict[key_name] = v
-        state_dict = new_state_dict
+    # state dict--------------------------------------------------------------------------
+    model_dict = network.state_dict()
+    new_state_dict = OrderedDict()
+    matched_layers, discarded_layers = [], []
 
-    #load part network in state dict-----------------------------------------------------------
-    for module in state_dict:
-        network.state_dict()[module][:] = state_dict[module]
+    # load model state ---->{matched_layers, discarded_layers}------------------------------------
+    for k, v in state_dict.items():
+        if k.startswith('module.'):
+            k = k[7:]  # discard module.
+
+        if k in model_dict and model_dict[k].size() == v.size():
+            new_state_dict[k] = v
+            matched_layers.append(k)
+        else:
+            discarded_layers.append(k)
+
+    model_dict.update(new_state_dict)
+    network.load_state_dict(model_dict)
+
+    # assert model state ------------------------------------------------------------------------
+    if len(matched_layers) == 0:
+        warnings.warn(
+            'The pretrained weights "{}" cannot be loaded, '
+            'please check the key names manually '
+            '(** ignored and continue **)'.format(weight_path)
+        )
+    else:
+        print(
+            'Successfully loaded pretrained weights from "{}"'.
+            format(path)
+        )
+        if len(discarded_layers) > 0:
+            print(
+                '** The following layers are discarded '
+                'due to unmatched keys or layer size: {}'.
+                format(discarded_layers)
+            )
 
     return network

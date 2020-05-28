83dade07f45fea0c276c72b65a1ac8530cbd4e1e
Huang Yin <1071324110@qq.com>
2020-05-28 17:37:15 +0800

    1

--- models/NoiseVectorizer.py
+++ models/NoiseVectorizer.py
@@ -9,22 +9,68 @@ class NoiseVectorizer(nn.Module):
     def __init__(self, emb):
         super().__init__()
 
-        self.net = nn.Sequential(
-            nn.Linear(emb, emb),
-            leaky_relu(0.2),
-            nn.Linear(emb, 512),
-            leaky_relu(0.2),
-            nn.Linear(512, 1024),
-            leaky_relu(0.2),
-            nn.Linear(1024, 2048),
-            leaky_relu(0.2),
-            nn.Linear(2048, 4096),
-            # nn.BatchNorm1d(4096),
+        self.layer_channel = [512, 256, 128, 64, 32]
+        layer_h_w = [4, 8, 16, 32, 64]
+
+        # --------------------------------------------------
+        layer_index = 0
+        self.layer0 = nn.Sequential(
+            nn.Linear(emb, self.layer_channel[layer_index]),
+            nn.BatchNorm1d(self.layer_channel[layer_index]),
             nn.Sigmoid()
         )
+        self.layer0_pool = nn.AdaptiveAvgPool2d((layer_h_w[layer_index], layer_h_w[layer_index]))
+
+        # --------------------------------------------------
+        layer_index = 1
+        self.layer1 = nn.Sequential(
+            nn.Linear(emb, self.layer_channel[layer_index]),
+            nn.BatchNorm1d(self.layer_channel[layer_index]),
+            nn.Sigmoid()
+        )
+        self.layer1_pool = nn.AdaptiveAvgPool2d((layer_h_w[layer_index], layer_h_w[layer_index]))
+
+        # --------------------------------------------------
+        layer_index = 2
+        self.layer2 = nn.Sequential(
+            nn.Linear(emb, self.layer_channel[layer_index]),
+            nn.BatchNorm1d(self.layer_channel[layer_index]),
+            nn.Sigmoid()
+        )
+        self.layer2_pool = nn.AdaptiveAvgPool2d((layer_h_w[layer_index], layer_h_w[layer_index]))
+
+        # --------------------------------------------------
+        layer_index = 3
+        self.layer3 = nn.Sequential(
+            nn.Linear(emb, self.layer_channel[layer_index]),
+            nn.BatchNorm1d(self.layer_channel[layer_index]),
+            nn.Sigmoid()
+        )
+        self.layer3_pool = nn.AdaptiveAvgPool2d((layer_h_w[layer_index], layer_h_w[layer_index]))
+
+        # --------------------------------------------------
+        layer_index = 4
+        self.layer4 = nn.Sequential(
+            nn.Linear(emb, self.layer_channel[layer_index]),
+            nn.BatchNorm1d(self.layer_channel[layer_index]),
+            nn.Sigmoid()
+        )
+        self.layer4_pool = nn.AdaptiveAvgPool2d((layer_h_w[layer_index], layer_h_w[layer_index]))
 
     def forward(self, x):
+        x0 = self.layer0(x).reshape(-1, self.layer_channel[0], 1, 1)
+        x0 = self.layer0_pool(x0)
 
-        # return torch.FloatTensor(x.size(0), 64, 64, 1).uniform_(0., 1.).to(device)
-        return self.net(x).reshape(-1, 64, 64, 1)
+        x1 = self.layer1(x).reshape(-1, self.layer_channel[1], 1, 1)
+        x1 = self.layer1_pool(x1)
 
+        x2 = self.layer2(x).reshape(-1, self.layer_channel[2], 1, 1)
+        x2 = self.layer2_pool(x2)
+
+        x3 = self.layer3(x).reshape(-1, self.layer_channel[3], 1, 1)
+        x3 = self.layer3_pool(x3)
+
+        x4 = self.layer4(x).reshape(-1, self.layer_channel[4], 1, 1)
+        x4 = self.layer4_pool(x4)
+
+        return [x0, x1, x2, x3, x4]
--- test_module/test_NV.py
+++ test_module/test_NV.py
@@ -1,13 +1,19 @@
 
 import sys
 sys.path.append('/home/hy/vscode/StyleGANStege')
-
-import torch
 from models.NoiseVectorizer import *
+import torch
+
+    
 
 
 
 if __name__ == "__main__":
-    m=NoiseVectorizer(100)
-    inp = torch.randn(3,100)
-    print(inp.shape)
+
+    m = NoiseVectorizer(100)
+    inp = torch.randn(3, 100)
+    # inp = torch.FloatTensor([[1,2,3,4],[5,6,7,8]])
+    out = m(inp)
+    for _ in range(5):
+        print(out[_].shape)
+    print('complete check.')

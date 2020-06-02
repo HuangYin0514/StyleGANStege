a8c71e27a40aad4289771a15ff3fa9eda8dd391e
Huang Yin <1071324110@qq.com>
2020-06-02 23:11:17 +0800

    1

--- models/NoiseVectorizer.py
+++ models/NoiseVectorizer.py
@@ -18,47 +18,50 @@ class NoiseVectorizer(nn.Module):
             nn.Linear(emb, self.layer_h_w[layer_index]),
             nn.BatchNorm1d(self.layer_h_w[layer_index]),
             nn.Sigmoid()
+            # nn.ReLU()
         )
         # --------------------------------------------------
         layer_index = 1
         self.layer1 = nn.Sequential(
-            nn.Linear(self.layer_h_w[layer_index-1], self.layer_h_w[layer_index]),
+            nn.Linear(emb, self.layer_h_w[layer_index]),
             nn.BatchNorm1d(self.layer_h_w[layer_index]),
             nn.Sigmoid()
+            # nn.ReLU()
         )
         # --------------------------------------------------
         layer_index = 2
         self.layer2 = nn.Sequential(
-            nn.Linear(self.layer_h_w[layer_index-1], self.layer_h_w[layer_index]),
+            nn.Linear(emb, self.layer_h_w[layer_index]),
             nn.BatchNorm1d(self.layer_h_w[layer_index]),
             nn.Sigmoid()
+            # nn.ReLU()
         )
         # --------------------------------------------------
         layer_index = 3
         self.layer3 = nn.Sequential(
-            nn.Linear(self.layer_h_w[layer_index-1], self.layer_h_w[layer_index]),
+            nn.Linear(emb, self.layer_h_w[layer_index]),
             nn.BatchNorm1d(self.layer_h_w[layer_index]),
             nn.Sigmoid()
+            # nn.ReLU()
         )
         # --------------------------------------------------
         layer_index = 4
         self.layer4 = nn.Sequential(
-            nn.Linear(self.layer_h_w[layer_index-1], self.layer_h_w[layer_index]),
+            nn.Linear(emb, self.layer_h_w[layer_index]),
             nn.BatchNorm1d(self.layer_h_w[layer_index]),
             nn.Sigmoid()
+            # nn.ReLU()
         )
 
     def forward(self, x):
-        x0 = self.layer0(x)
-        x1 = self.layer1(x0)
-        x2 = self.layer2(x1)
-        x3 = self.layer3(x2)
-        x4 = self.layer4(x3)
+        x0 = self.layer0(x).reshape(-1, 4, 4, 1)
 
-        x0 = x0.reshape(-1, 4, 4, 1)
-        x1 = x1.reshape(-1, 8, 8, 1)
-        x2 = x2.reshape(-1, 16, 16, 1)
-        x3 = x3.reshape(-1, 32, 32, 1)
-        x4 = x4.reshape(-1, 64, 64, 1)
+        x1 = self.layer1(x).reshape(-1, 8, 8, 1)
+
+        x2 = self.layer2(x).reshape(-1, 16, 16, 1)
+
+        x3 = self.layer3(x).reshape(-1, 32, 32, 1)
+
+        x4 = self.layer4(x).reshape(-1, 64, 64, 1)
 
         return [0.5*x0, 0.5*x1, 0.5*x2, 0.5*x3, 0.5*x4]

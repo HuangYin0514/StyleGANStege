69d27a3c425eee7c561e79a42aea4fdc6a54d196
Huang Yin <1071324110@qq.com>
2020-09-14 18:53:54 +0800

    1

--- models/FineTuneStylegan.py
+++ models/FineTuneStylegan.py
@@ -41,8 +41,8 @@ class FineTuneStylegan(nn.Module):
         base_param_ids = set(map(id, self.E.to_logit.parameters()))
         new_params = [p for p in self.E.parameters() if id(p) not in base_param_ids]
         E_param_groups = [{'params': self.E.parameters(), 'lr': self.lr},
-                          {'params': self.G.downsample.parameters(), 'lr': self.lr},
-                          {'params': new_params, 'lr': self.lr},  # other E layers
+                          {'params': self.G.parameters(), 'lr': self.lr},
+                        #   {'params': new_params, 'lr': self.lr},  # other E layers
                           {'params': self.N.parameters(), 'lr': self.lr},
                           ]
         self.E_opt = DiffGrad(E_param_groups, lr=self.lr, betas=(0.5, 0.9))
